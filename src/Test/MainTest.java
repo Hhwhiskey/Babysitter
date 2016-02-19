e1e34078aa8a74d13351852647c4390f3cc32e99
Kevin <khfire22@gmail.com>
2016-02-18 20:44:57 -0500

    testDayRate fail

--- src/Test/MainTest.java
+++
@@ -1,7 +0,0 @@
-package Test;
-
-/**
- * Created by Kevin on 2/18/2016.
- */
-public class MainTest {
-}
---
+++ src/com/kevinhodges/Babysitter.java
@@ -0,0 +1,36 @@
+package com.kevinhodges;
+
+import java.text.SimpleDateFormat;
+
+public class Babysitter {
+
+    int dayRate;
+    int nightRate;
+    int lateRate;
+    int shiftLengthMax;
+    int shiftLengthMin;
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH");
+
+    public static void main(String[] args) {
+    }
+
+    public int getDayRate() {
+        return dayRate;
+    }
+
+    public int getNightRate() {
+        return nightRate;
+    }
+
+    public int getLateRate() {
+        return lateRate;
+    }
+
+    public int getShiftLengthMax() {
+        return shiftLengthMax;
+    }
+
+    public int getShiftLengthMin() {
+        return shiftLengthMin;
+    }
+}
