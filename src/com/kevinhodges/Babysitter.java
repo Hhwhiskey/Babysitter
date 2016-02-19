1279ffad5489cfde482683bfcf7dfdf719d02bbf
Kevin <khfire22@gmail.com>
2016-02-19 01:17:37 -0500

    redo of testCalculateShiftLength, I had crucial code in my test instead of my method

--- src/com/kevinhodges/Babysitter.java
+++ src/com/kevinhodges/Babysitter.java
@@ -2,6 +2,7 @@ package com.kevinhodges;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class Babysitter {
 
@@ -18,7 +19,30 @@ public class Babysitter {
 
     public int calculateShiftLength(Calendar shiftStartDate, Calendar shiftEndDate) {
 
-        int shiftLengthHours = 0;
+        // Create calendar date with time of 1700 on feb 19th
+        shiftStartDate.set(Calendar.YEAR, 2016);
+        shiftStartDate.set(Calendar.MONTH, 2);
+        shiftStartDate.set(Calendar.DATE, 19);
+        shiftStartDate.set(Calendar.HOUR_OF_DAY, 17);
+
+        // Convert shiftStartDate to millis
+        long startingTime = shiftStartDate.getTimeInMillis();
+
+        // Create calendar date with time of 0400 on feb 20th
+        shiftEndDate.set(Calendar.YEAR, 2016);
+        shiftEndDate.set(Calendar.MONTH, 2);
+        shiftEndDate.set(Calendar.DATE, 20);
+        shiftEndDate.set(Calendar.HOUR_OF_DAY, 4);
+
+        // Convert shiftEndDate to millis
+        long quittingTime = shiftEndDate.getTimeInMillis();
+
+        // Subtract the starting time from quitting time to get the
+        // length of the shift in millis
+        long shiftLength = quittingTime - startingTime;
+
+        // Convert the millis time to hours, rounded up to the hour.
+        int shiftLengthHours = (int) TimeUnit.MILLISECONDS.toHours(shiftLength);
 
         return shiftLengthHours;
     }
